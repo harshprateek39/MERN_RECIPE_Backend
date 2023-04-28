f263417dc62900c360b719cfe560f19ac8fcc455
PREDATOR <piscesscorpio11819@gmail.com>
2023-04-28 15:57:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+import {userRouter} from "./src/routes/users.js"
+import { recipeRouter } from "./src/routes/recipees.js";
+dotenv.config();
+const app=express();
+app.use(express.json());
+app.use(cors());
+app.get('/', function (req, res) {
+    res.send('Api is working!');
+ })
+app.use("/auth",userRouter);
+app.use("/recipes",recipeRouter);
+mongoose.connect(process.env.MongoURL) 
+ 
+app.listen(3001,()=>{console.log("started");});
+   
